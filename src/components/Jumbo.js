44167f02a31bba72693cb5e3012e80641c447fbf
Rahilcode <raheelkhatttal@gmail.com>
2023-04-13 15:14:17 +0530

    jumbotron component added

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -3,10 +3,8 @@ import Carousel from "react-bootstrap/Carousel";
 const Jumbo = () => {
   return (
     <div>
-      {/* <Carousel> */}
-      {/* <Carousel.Item> */}
       <img
-        style={{ height: "90vh" }}
+        style={{ height: "90vh", objectFit: "cover" }}
         className="d-block w-100"
         src="https://img.freepik.com/free-vector/son-father-home-library-son-busy-dad_107791-8661.jpg?size=626&ext=jpg"
         alt="jumbotron"
@@ -15,8 +13,6 @@ const Jumbo = () => {
         <h3>Explore the latest Articles</h3>
         <p>Be Up To Date with what is going on around the world.</p>
       </Carousel.Caption>
-      {/* </Carousel.Item> */}
-      {/* </Carousel> */}
     </div>
   );
 };
