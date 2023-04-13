baaa25fa6a1ea87753c62c89b6fa633540b733bc
Rahilcode <raheelkhatttal@gmail.com>
2023-04-13 15:23:20 +0530

    edited file

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: "fir-blog-1c446",
-  storageBucket: "fir-blog-1c446.appspot.com",
-  messagingSenderId: "849024449317",
-  appId: "1:849024449317:web:bc6fda88a6729a15e2f8cc",
+  apiKey: ,
+  authDomain: ,
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
